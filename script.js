1c5ebd316735ac86716095775ce83f5c5af6b4ed
jimrom66 <188324624+jimrom66@users.noreply.github.com>
2025-01-12 13:47:08 +0200

    have to fix division, underscore

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const numberButtons= document.querySelectorAll(".number-button");
 const actionButtons= document.querySelectorAll(".action-button");
 const clearButton= document.querySelector(".clear-btn");
 const operateButton= document.querySelector(".operate-button");
+const backButton= document.querySelector(".backspace-button");
+const floatButton=document.querySelector(".float-button");
+let firstNumber,operator,secondNumber,result;
 
 
 numberButtons.forEach(function(button) {
@@ -15,31 +18,53 @@ numberButtons.forEach(function(button) {
 actionButtons.forEach(function(button) {
     button.addEventListener("click", function() {
         const buttonValue = this.getAttribute("value");  
-        input.value += buttonValue;  
+        firstNumber= parseFloat(input.value);
+        input.value=buttonValue;  
+        operator=buttonValue;
+        disableButtons(actionButtons);
     });
 });
 
 clearButton.addEventListener("click", function(){
     input.value="";
+    firstNumber=0;
+    operator=0;
+    secondNumber=0;
+    result=0;
+    enableButtons(actionButtons);
+    floatButton.disabled=false;
 })
 
-operateButton.addEventListener("click",function(){
-    let firstNumber,secondNumber,result;
-    const expression = input.value.trim()
-    if (expression.includes('+')) {
-        [firstNumber, secondNumber] = expression.split('+');
-        result=addition(parseFloat(firstNumber),parseFloat(secondNumber));
-    } else if (expression.includes('-')) {
-        [firstNumber, secondNumber] = expression.split('-');
-        result=substract(parseFloat(firstNumber),parseFloat(secondNumber));
-    } else if (expression.includes('*')) {
-        [firstNumber, secondNumber] = expression.split('*');
-       result=multiplication(parseFloat(firstNumber),parseFloat(secondNumber));
-    } else if (expression.includes('/')) {
-        [firstNumber, secondNumber] = expression.split('/');
-        result=divide(parseFloat(firstNumber),parseFloat(secondNumber));
+floatButton.addEventListener("click",function(){
+    const buttonValue= this.getAttribute("value");
+    input.value += buttonValue;
+    floatButton.disabled=true;
+})
+
+backButton.addEventListener("click", function(){
+    input.value=input.value.slice(0,-1);
+})
+
+operateButton.addEventListener("click", function(){
+    secondNumber=parseFloat(input.value.slice(1));
+    if (operator=="+"){
+        result=addition(firstNumber,secondNumber);
     }
-    input.value=result
+    else if (operator=="-"){
+        result=substract(firstNumber,secondNumber);
+    }
+    else if (operator=="*"){
+        result=multiplication(firstNumber,secondNumber);
+    }
+    else if (operator=="/"){
+        result=divide(firstNumber.secondNumber);
+    }
+    input.value=result;
+    firstNumber=result;
+    enableButtons(actionButtons)
+
+
+
 
 })
 
@@ -51,3 +76,14 @@ const substract=(firstNumber, secondNumber)=>firstNumber-secondNumber;
 const multiplication=(firstNumber, secondNumber)=>firstNumber*secondNumber;
 const divide = (firstNumber, secondNumber) => secondNumber !== 0 ? firstNumber / secondNumber : 'Error: Division by zero';
 
+function disableButtons(typeOfButton){
+    typeOfButton.forEach(button=>{
+        button.disabled=true;
+    })
+}
+
+function enableButtons(typeOfButton){
+    typeOfButton.forEach(button => {
+        button.disabled=false;
+    })
+}
